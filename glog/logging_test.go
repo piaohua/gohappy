8fafdc7f8243a537a11f8a24287cd60228c595c0
piaohua <twopiao7@gmail.com>
2018-05-16 16:54:34 +0800

    add glog

---
+++ glog/logging_test.go
@@ -0,0 +1,15 @@
+package glog
+
+import "testing"
+
+func TestLog(t *testing.T) {
+	Init()
+	defer Flush()
+	//go run logging.go -log_dir=.
+	log.Debugf("debug %s", Password("secret"))
+	log.Info("info")
+	log.Notice("notice")
+	log.Warning("warning")
+	log.Error("err")
+	log.Critical("crit")
+}
